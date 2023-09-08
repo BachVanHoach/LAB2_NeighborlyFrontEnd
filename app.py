3c9a19541424e944b022d7a8fd4c2cf508424dbd
BachVanHoach <117200521+BachVanHoach@users.noreply.github.com>
2023-09-08 11:23:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def rss():
 
     response = requests.get(settings.API_URL + '/getAdvertisements')
     ads = response.json()
-    print("ads:" + ads)
+    print(ads)
     for a in ads: 
         fe = fg.add_entry()
         fe.title(a.title)
